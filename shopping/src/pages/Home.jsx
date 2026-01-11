90b255eefa2e4a1410a19741fe0bde3e1f71b632
Anthony <anthony.matheusfrz@gmail.com>
2026-01-11 15:03:18 -0300

    .

--- shopping/src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <section className="hero">
-      <h2>Viva a melhor experiência</h2>
-      <p>Compras, lazer, gastronomia e serviços em um só lugar.</p>
-    </section>
-  );
-}
